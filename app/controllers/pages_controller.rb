b38c194e251e65092005728a3c27fd667222a72c
varshnimurali <vmurali0@chicagobooth.edu>
2025-04-29 19:20:00 +0000

    save updates

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def home
+    @list_of_items = Item.all
     render({ :template => "pages_template/home" })
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
 
-  get("/home", { :controller => "pages", :action => "home"})
+  get("/", { :controller => "pages", :action => "home"})
   
 end
